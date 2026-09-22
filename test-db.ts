6d1d8cc4fe7ac52ee2a39de21e7a6c50fbb1b5b8
cheerysaaru <cheerysaark@gmail.com>
2026-09-23 03:59:28 +0530

    Add .gitignore for SQLite and test files

--- test-db.ts
+++
@@ -1,8 +0,0 @@
-import { PrismaClient } from '@prisma/client';
-const prisma = new PrismaClient();
-async function main() {
-  const tables = await prisma.$queryRaw`SELECT name FROM sqlite_master WHERE type='table'`;
-  console.log(tables);
-  await prisma.$disconnect();
-}
-main();
